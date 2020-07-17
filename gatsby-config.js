31025ea3dd101660f5ae27382de7f40c9038510a
Haji Mohammed <hajisy07@gmail.com>
2020-07-17 15:26:57 +0100

    updated gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/recipes`,
+        name: 'recipes',
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
