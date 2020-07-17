d26f87ae80f9cfb452b2c5474c17b5ed6980c81b
Haji Mohammed <hajisy07@gmail.com>
2020-07-17 17:14:47 +0100

    added gatsby config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,49 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
 
-// You can delete this file if you're not using it
+
+// exports.createPages = async({ actions, graphql, reporter }) => {
+//   const { createPage } = actions
+//   const RecipeTemplate = require.resolve('./src/templates/recipeTemplates.js')
+
+//   const reult = await qraphql(`
+//     {
+//       allMarkdownRemark(limis: 1000){
+//         edges {
+//           node {
+//             id
+//             fields {
+//               slug
+//             }
+//             frontmatter{
+//               tags
+//               title
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then((result) => {
+//     if(result.errors){
+//       results.errors.forEach(e => console.error(e.toString()))
+//       return Promise.reject(result.errors)
+//     }
+
+//     console.log(result)
+
+//   })
+// }
+
+
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  fmImagesToRelative(node) // convert image paths for gatsby images
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
