4f632aa45347f9c16cfe59aa0ca9b1f7ce7948f6
Haji Mohammed <hajisy07@gmail.com>
2020-07-20 17:25:42 +0100

    layout for tags

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -2,20 +2,34 @@ import React from 'react'
 import Layout from "../components/layout"
 import { kebabCase } from 'lodash'
 import { graphql, Link } from 'gatsby'
+import SEO from "../components/seo"
+import Header from "../components/header"
+import Sidenav from "../components/sidenav"
+import SideNavProvider from "../components/sideNavProvider"
 
 function TagList({ tags }) {
-  return tags.map((tag, i) => <li key={i}>
-    <Link to={`/tags/${kebabCase(tag.fieldValue)}`}> {tag.fieldValue}</Link>
+  return tags.map((tag, i) => <li className="list-none text-3xl font-semibold " key={i}>
+    <Link to={`/tag/${kebabCase(tag.fieldValue)}`}> {tag.fieldValue}</Link>
   </li>)
 }
 
 const Tags = ({ data }) => {
 
   const tags = data.allMarkdownRemark.group
+  const siteTitle = data.site.siteMetadata.title;
 
   return (
     <Layout>
-      <TagList tags={tags} />
+      <SEO title="Tags" />
+      <SideNavProvider>
+        <Sidenav siteTitle={siteTitle} />
+        <Header title="Tags" siteTitle={siteTitle} />
+      </SideNavProvider>
+      <section className="h-screen">
+        <div className="divide-y divide-gray-400">
+          <TagList tags={tags} />
+        </div>
+      </section>
     </Layout>
   )
 }
@@ -23,7 +37,12 @@ const Tags = ({ data }) => {
 export default Tags
 
 export const TagsQuery = graphql`
-  query TagsQuery {
+  query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
     allMarkdownRemark {
       group(field: frontmatter___tags) {
         fieldValue
