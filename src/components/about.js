ef54c41c55bc052e23859b639a3982a50114b805
Haji Mohammed <hajisy07@gmail.com>
2020-08-03 20:53:21 +0100

    changed about query

--- src/components/about.js
+++ src/components/about.js
@@ -4,17 +4,24 @@ import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 export default function About() {
+
   const data = useStaticQuery(graphql`
   query {
-    sectionImage: file(relativePath: { eq: "profile.jpg" }) {
-      childImageSharp {
-        fixed(width: 300, height: 300) {
-          ...GatsbyImageSharpFixed
+    markdownRemark(frontmatter: {Layout: {eq: "about"}}) {
+      frontmatter {
+        about
+        thumbnail {
+          childImageSharp {
+            fixed(width: 300, height: 300) {
+              ...GatsbyImageSharpFixed
+            }
+          }
         }
       }
     }
   }
 `)
+  console.log(data)
 
   return (
     <>
@@ -23,13 +30,16 @@ export default function About() {
         <h5 className="sub-heading text-white">ABOUT</h5>
         <div className="w-full flex justify-between items-center flex-col md:flex-row mt-16">
           <div className="image-container flex-shrink-0 w-w300 h-h300">
-            <Img className="h-full w-full rounded-circle" fixed={data.sectionImage.childImageSharp.fixed} objectFit="cover" />
+            <Img className="h-full w-full rounded-circle" fixed={data.markdownRemark.frontmatter.thumbnail.childImageSharp.fixed} objectFit="cover" />
           </div>
           <div className="flex w-full flex-col justify-center items-center my-5 md:pl-10">
-            <p className="my-3 text-white text-center md:text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ligula nisi, cursus sit amet hendrerit nec, porta ut velit. Maecenas tristique, dui sit amet sollicitudin sagittis, ante metus blandit nisi, nec interdum sapien lorem eu purus. Phasellus efficitur ligula ante, vitae congue tortor consequat at. Quisque ullamcorper vitae lacus dignissim mollis. Duis dignissim tellus quis nisl fringilla vehicula. Etiam placerat</p>
+            <p className="my-3 text-white text-center md:text-left">
+              {data.markdownRemark.frontmatter.about}
+            </p>
           </div>
         </div>
       </section>
     </>
   )
 }
+
